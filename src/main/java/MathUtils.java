5fc7a71d9047a2e490df0dfc712a460c9e4c5f0d
funtikova <funtikova95@mail.ru>
2020-09-02 00:13:43 +0500

    2 cmt

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -1,7 +1,32 @@
+import static java.lang.Math.PI;
+
 public class MathUtils {
 
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+
+
+    public int[] arr(int a, int b, int c, int d) {
+        int[] z = new int[]{a, b, c, d};
+        return z;
+    }
+
+    public double computeCircleArea(double radius) {
+        return PI * radius * radius;
     }
 
 }
--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,17 +1,75 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
+
+/*
+20
+ */
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
+    MathUtils mathUtils;
 
-    @DisplayName("01 Test add")
-    @Test
-    void add() {
-        MathUtils mathUtils = new MathUtils();
-        int expected = 2;
-        int actual = mathUtils.add(1,1);
-        assertEquals(expected,actual);
-
+    @BeforeAll
+    static void beforeAllInit() {
+        System.out.println("This needs to run before all");
     }
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanUp() {
+        System.out.println("Test passed");
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    @DisplayName("01 Test add")
+    void addTest() {
+
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+        assertEquals(expected, actual);
+    }
+
+    @EnabledOnJre(JRE.JAVA_10)
+    @Test
+    @DisplayName("02 Test Array")
+    void arrTest() {
+        int[] expected = {1, 2, 3, 4};
+        int[] actual = mathUtils.arr(1, 2, 3, 4);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("03 Test Circle")
+    void computeCircleAreaTest() {
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10),
+                "Should return right circle area");
+    }
+
+    @Test
+    @DisplayName("04 Test Divide")
+    void divideTest() {
+        boolean value = true;
+        assumeTrue(value);
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
+                "Divide by zero should be throw");
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("TDD method. Should not run")
+    void testDisabled() {
+        fail("This test should be disabled");
+    }
+
 }
