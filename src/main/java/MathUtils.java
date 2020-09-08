fe27d5c0a4290127bf675a0f0fd53916be622ef6
funtikova <funtikova95@mail.ru>
2020-09-08 21:19:24 +0500

    Revert "26 TestInfo, TestReporter"
    This reverts commit bcbd14dd

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -1,7 +1,35 @@
+import javafx.beans.binding.Bindings;
+
+import java.util.*;
+
+import static java.lang.Math.PI;
+
 public class MathUtils {
 
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+
+
+    public int[] arr(int a, int b, int c, int d) {
+        return new int[]{a, b, c, d};
+    }
+
+    public double computeCircleArea(double radius) {
+        return PI * radius * radius;
     }
 
 }
